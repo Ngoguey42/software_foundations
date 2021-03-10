1f4ff7fad04a34eb2f22906c5daba2d71c54e156
Ngoguey42 <ngoguey@student.42.fr>
2021-03-10 22:08:55 +0100

    late commit

--- lf/MyInduction.v
+++ lf/MyInduction.v
@@ -285,3 +285,142 @@ Proof.
 Qed.
 
 (* ************************************************************************** *)
+(* Exercise: 3 stars, standard, especially useful (binary_commute) *)
+Inductive bin : Type :=
+| Z
+| B0 (n : bin)
+| B1 (n : bin)
+.
+
+  (* | B1 (B0 Z) => B1 (B1 Z) *)
+  (* | B1 Z      => B1 (B1 Z) *)
+  (* | B1 (B1 m') => incr B1 m' *)
+
+Definition zero :=                      Z.
+Definition zero' :=                  B0 Z.
+Definition zero'' :=             B0 (B0 Z).
+Definition one :=                    B1 Z.
+Definition one' :=               B1 (B0 Z).
+Definition one'' :=          B1 (B0 (B0 Z)).
+Definition two :=                B0 (B1 Z).
+Definition two' :=           B0 (B1 (B0 Z)).
+Definition two'' :=      B0 (B1 (B0 (B0 Z))).
+Definition three :=              B1 (B1 Z).
+Definition three' :=         B1 (B1 (B0 Z)).
+Definition three'' :=    B1 (B1 (B0 (B0 Z))).
+Definition four :=           B0 (B0 (B1 Z)).
+Definition four' :=      B0 (B0 (B1 (B0 Z))).
+Definition four'' := B0 (B0 (B1 (B0 (B0 Z)))).
+
+Inductive parent : Type :=
+| Root
+| All1
+| Some0
+.
+
+Inductive child : Type :=
+| All0
+| Some1
+.
+
+(* Fixpoint _incr (all_ones_before : bool) (m : bin) : (bool * bin) := *)
+(*   match all_ones_before, m with *)
+(*   | true, B1 m' => *)
+(*     (match _incr true m' with *)
+(*     | true => (true, Z) *)
+(*     | false => (true, Z) *)
+(*     end) *)
+(*   | true, _ => (true, Z) *)
+(*   | false, _ => (true, Z) *)
+(*   end. *)
+
+Fixpoint _incr (p : parent) (m : bin) : (bin * child) :=
+  match p, m with
+  | Root, B1 m' =>
+    match _incr All1 m' with
+    | (n, All0) => (Z, Some1) (* absurd *)
+    | (n, Some1) => (Z, Some1)
+    end
+  | All1, B1 m' =>
+    match _incr All1 m' with
+    | (n, All0) => (Z, Some1)
+    | (n, Some1) => (Z, Some1)
+    end
+  | Some0, B1 m' =>
+    match _incr Some0 m' with
+    | (n, All0) => (Z, Some1)
+    | (n, Some1) => (Z, Some1)
+    end
+  | Root, B0 m' =>
+    match _incr Some0 m' with
+    | (n, All0) => (Z, Some1)
+    | (n, Some1) => (Z, Some1)
+    end
+  | All1, B0 m' =>
+    match _incr Some0 m' with
+    | (n, All0) => (Z, Some1)
+    | (n, Some1) => (Z, Some1)
+    end
+  | Some0, B0 m' =>
+    match _incr Some0 m' with
+    | (n, All0) => (Z, Some1)
+    | (n, Some1) => (Z, Some1)
+    end
+  | Root, Z => (B1 Z, Some1) (* 0 + 1 *)
+  | All1, Z => (B1 Z, Some1)
+  | Some0, Z => (Z, All0)
+
+  end.
+
+Fixpoint incr (m : bin) : bin :=
+  match m with
+  | Z => B1 Z
+  | B0 m' => B1 m'
+  | B1 m' => B0 (B1 m)
+  end.
+Example test0 : (incr zero) = one. Proof. reflexivity. Qed.
+Example test1 : (incr one) = two. Proof. compute. reflexivity. Qed.
+Example test2 : (incr two) = three. Proof. reflexivity. Qed.
+Example test3 : (incr three) = four. Proof. reflexivity. Qed.
+
+Fixpoint _bin_to_nat (m : bin) (weight : nat) : nat :=
+  match m with
+  | Z => O
+  | B1 m' => weight + _bin_to_nat m' (weight * 2)
+  | B0 m' => _bin_to_nat m' (weight * 2)
+  end.
+Definition bin_to_nat (m : bin) : nat := _bin_to_nat m 1.
+
+
+Example test0 : bin_to_nat zero = 0. Proof. reflexivity. Qed.
+Example test1 : bin_to_nat one = 1. Proof. reflexivity. Qed.
+Example test2 : bin_to_nat two = 2. Proof. reflexivity. Qed.
+Example test3 : bin_to_nat three = 3. Proof. reflexivity. Qed.
+Example test4 : bin_to_nat four = 4. Proof. reflexivity. Qed.
+
+
+Example test1 : bin_to_nat (incr Z) = 1. Proof. reflexivity. Qed.
+Example test1' : bin_to_nat (B1 Z) = 1. Proof. reflexivity. Qed.
+Example test2 : bin_to_nat (incr (incr Z)) = 2. Proof. reflexivity. Qed.
+Example test2' : bin_to_nat (incr (B1 Z)) = 2. Proof. reflexivity. Qed.
+Example test2'' : bin_to_nat (B0 (B1 Z)) = 2. Proof. reflexivity. Qed.
+
+Theorem bin_to_nat_pres_incr : forall b : bin, bin_to_nat (incr b) = S (bin_to_nat b).
+Proof.
+  intros b.
+  destruct b.
+  - simpl. compute. reflexivity.
+  - simpl.
+
+    replace (bin_to_nat Z) with 0.
+    replace (bin_to_nat (B0 Z)) with 1.
+    reflexivity.
+    compute.
+
+
+Qed.
+
+Theorem truc : forall b
+
+
+(* ************************************************************************** *)
