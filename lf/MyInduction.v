ac31932706a0d078fabe5c30d0d1765a36c284fe
ngoguey42 <ngoguey42@free.fr>
2022-09-30 18:03:32 +0200

    Progress in Induction

--- lf/MyInduction.v
+++ lf/MyInduction.v
@@ -51,11 +51,37 @@ Proof.
   intros n.
   induction n as [|n' IHn'].
   - simpl. reflexivity.
-  - simpl. rewrite -> IHn'. simpl. rewrite -> plus_n_Sm. reflexivity.
+  - simpl. rewrite -> IHn'. rewrite -> plus_n_Sm. reflexivity.
+Qed.
+
+(* ************************************************************************** *)
+(* Exercise: 2 stars, standard (eqb_refl) *)
+Fixpoint eqb (n m : nat) : bool :=
+  match n with
+  | O => match m with
+         | O => true
+         | S m' => false
+         end
+  | S n' => match m with
+            | O => false
+            | S m' => eqb n' m'
+            end
+  end.
+
+Notation "x =? y" := (eqb x y) (at level 70) : nat_scope.
+
+Theorem eqb_refl : forall n : nat,
+  (n =? n) = true.
+Proof.
+  intros n.
+  induction n as [|n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
 Qed.
 
 (* ************************************************************************** *)
 (* Exercise: 2 stars, standard, optional (evenb_S) *)
+
 Fixpoint evenb (n : nat) : bool :=
   match n with
   | O => true
@@ -159,15 +185,15 @@ Fixpoint leb (n m : nat) : bool :=
   | O, O | O, S _ => true
   | S _, O => false
   | S n', S m' => leb n' m'
-end.
-Fixpoint eqb (n m : nat) : bool :=
+  end.
+Fixpoint eqb' (n m : nat) : bool :=
   match n, m with
   | O, O => true
-  | S n', S m' => eqb n' m'
+  | S n', S m' => eqb' n' m'
   | _, _ => false
 end.
 Notation "x <=? y" := (leb x y) (at level 70) : nat_scope.
-Notation "x =? y" := (eqb x y) (at level 70) : nat_scope.
+Notation "x =? y" := (eqb' x y) (at level 70) : nat_scope.
 Theorem leb_refl : forall n : nat, true = (n <=? n).
 Proof.
   intros n.
@@ -261,16 +287,6 @@ Proof.
 Qed.
 
 (* ************************************************************************** *)
-(* Exercise: 2 stars, standard, optional (eqb_refl) *)
-Theorem eqb_refl : forall n : nat, true = (n =? n).
-Proof.
-  intros n.
-  induction n as [|n' IHn'].
-  - reflexivity.
-  - simpl. rewrite <- IHn'. reflexivity.
-Qed.
-
-(* ************************************************************************** *)
 (* Exercise: 2 stars, standard, optional (plus_swap') *)
 Theorem plus_swap' : forall n m p : nat, n + (m + p) = m + (n + p).
 Proof.
@@ -291,136 +307,113 @@ Inductive bin : Type :=
 | B0 (n : bin)
 | B1 (n : bin)
 .
-
-  (* | B1 (B0 Z) => B1 (B1 Z) *)
-  (* | B1 Z      => B1 (B1 Z) *)
-  (* | B1 (B1 m') => incr B1 m' *)
-
-Definition zero :=                      Z.
-Definition zero' :=                  B0 Z.
-Definition zero'' :=             B0 (B0 Z).
-Definition one :=                    B1 Z.
-Definition one' :=               B1 (B0 Z).
-Definition one'' :=          B1 (B0 (B0 Z)).
-Definition two :=                B0 (B1 Z).
-Definition two' :=           B0 (B1 (B0 Z)).
-Definition two'' :=      B0 (B1 (B0 (B0 Z))).
-Definition three :=              B1 (B1 Z).
-Definition three' :=         B1 (B1 (B0 Z)).
-Definition three'' :=    B1 (B1 (B0 (B0 Z))).
-Definition four :=           B0 (B0 (B1 Z)).
-Definition four' :=      B0 (B0 (B1 (B0 Z))).
-Definition four'' := B0 (B0 (B1 (B0 (B0 Z)))).
-
-Inductive parent : Type :=
-| Root
-| All1
-| Some0
-.
-
-Inductive child : Type :=
-| All0
-| Some1
-.
-
-(* Fixpoint _incr (all_ones_before : bool) (m : bin) : (bool * bin) := *)
-(*   match all_ones_before, m with *)
-(*   | true, B1 m' => *)
-(*     (match _incr true m' with *)
-(*     | true => (true, Z) *)
-(*     | false => (true, Z) *)
-(*     end) *)
-(*   | true, _ => (true, Z) *)
-(*   | false, _ => (true, Z) *)
-(*   end. *)
-
-Fixpoint _incr (p : parent) (m : bin) : (bin * child) :=
-  match p, m with
-  | Root, B1 m' =>
-    match _incr All1 m' with
-    | (n, All0) => (Z, Some1) (* absurd *)
-    | (n, Some1) => (Z, Some1)
-    end
-  | All1, B1 m' =>
-    match _incr All1 m' with
-    | (n, All0) => (Z, Some1)
-    | (n, Some1) => (Z, Some1)
-    end
-  | Some0, B1 m' =>
-    match _incr Some0 m' with
-    | (n, All0) => (Z, Some1)
-    | (n, Some1) => (Z, Some1)
-    end
-  | Root, B0 m' =>
-    match _incr Some0 m' with
-    | (n, All0) => (Z, Some1)
-    | (n, Some1) => (Z, Some1)
-    end
-  | All1, B0 m' =>
-    match _incr Some0 m' with
-    | (n, All0) => (Z, Some1)
-    | (n, Some1) => (Z, Some1)
-    end
-  | Some0, B0 m' =>
-    match _incr Some0 m' with
-    | (n, All0) => (Z, Some1)
-    | (n, Some1) => (Z, Some1)
-    end
-  | Root, Z => (B1 Z, Some1) (* 0 + 1 *)
-  | All1, Z => (B1 Z, Some1)
-  | Some0, Z => (Z, All0)
-
-  end.
-
 Fixpoint incr (m : bin) : bin :=
   match m with
   | Z => B1 Z
-  | B0 m' => B1 m'
-  | B1 m' => B0 (B1 m)
+  | B0 n => B1 n
+  | B1 n => B0 (incr n)
   end.
-Example test0 : (incr zero) = one. Proof. reflexivity. Qed.
-Example test1 : (incr one) = two. Proof. compute. reflexivity. Qed.
-Example test2 : (incr two) = three. Proof. reflexivity. Qed.
-Example test3 : (incr three) = four. Proof. reflexivity. Qed.
-
-Fixpoint _bin_to_nat (m : bin) (weight : nat) : nat :=
+Fixpoint bin_to_nat (m : bin) : nat :=
   match m with
   | Z => O
-  | B1 m' => weight + _bin_to_nat m' (weight * 2)
-  | B0 m' => _bin_to_nat m' (weight * 2)
+  | B1 m' => 2 * (bin_to_nat m') + 1
+  | B0 m' => 2 * (bin_to_nat m')
   end.
-Definition bin_to_nat (m : bin) : nat := _bin_to_nat m 1.
-
-
-Example test0 : bin_to_nat zero = 0. Proof. reflexivity. Qed.
-Example test1 : bin_to_nat one = 1. Proof. reflexivity. Qed.
-Example test2 : bin_to_nat two = 2. Proof. reflexivity. Qed.
-Example test3 : bin_to_nat three = 3. Proof. reflexivity. Qed.
-Example test4 : bin_to_nat four = 4. Proof. reflexivity. Qed.
-
-
-Example test1 : bin_to_nat (incr Z) = 1. Proof. reflexivity. Qed.
-Example test1' : bin_to_nat (B1 Z) = 1. Proof. reflexivity. Qed.
-Example test2 : bin_to_nat (incr (incr Z)) = 2. Proof. reflexivity. Qed.
-Example test2' : bin_to_nat (incr (B1 Z)) = 2. Proof. reflexivity. Qed.
-Example test2'' : bin_to_nat (B0 (B1 Z)) = 2. Proof. reflexivity. Qed.
-
-Theorem bin_to_nat_pres_incr : forall b : bin, bin_to_nat (incr b) = S (bin_to_nat b).
+Theorem bin_to_nat_pres_incr : forall b : bin,
+    bin_to_nat (incr b) = 1 + bin_to_nat b.
 Proof.
+  assert (H: forall a, S a = a + 1). {
+    intros a.
+    induction a as [|a IHa].
+    - reflexivity.
+    - simpl. rewrite <- IHa. reflexivity.
+  }
   intros b.
-  destruct b.
-  - simpl. compute. reflexivity.
-  - simpl.
-
-    replace (bin_to_nat Z) with 0.
-    replace (bin_to_nat (B0 Z)) with 1.
-    reflexivity.
-    compute.
-
-
+  induction b as [|b IHb|b IHb].
+  - reflexivity.
+  - {
+      simpl.
+      rewrite <- H.
+      reflexivity.
+    }
+  - {
+      simpl.
+      rewrite -> IHb.
+      simpl.
+      rewrite <- plus_n_O.
+      rewrite <- plus_assoc.
+      rewrite <- H.
+      reflexivity.
+    }
 Qed.
 
-Theorem truc : forall b
+(* ************************************************************************** *)
+(* Exercise: 3 stars, standard (nat_bin_nat) *)
+Fixpoint nat_to_bin (x : nat) : bin :=
+  match x with
+  | O => Z
+  | S x => incr (nat_to_bin x)
+  end.
+Theorem incr_and_nat_to_bin : forall n, nat_to_bin (S n) = incr (nat_to_bin n).
+Proof.
+  intros n.
+  induction n as [|n IHn].
+  - reflexivity.
+  - { simpl. reflexivity. }
+Qed.
+Theorem nat_bin_nat : forall n, bin_to_nat (nat_to_bin n) = n.
+Proof.
+  intros n.
+  induction n as [|n IHn].
+  - reflexivity.
+  - {
+      rewrite -> incr_and_nat_to_bin.
+      rewrite -> bin_to_nat_pres_incr.
+      rewrite -> IHn.
+      reflexivity.
+    }
+Qed.
 
+(* ************************************************************************** *)
+(* Exercise: 2 stars, advanced (double_bin) *)
+(* plus_n_Sm: forall n m : nat, S (n + m) = n + S m *)
+(* plus_Sn_m: forall n m : nat, S n + m = S (n + m) *)
+Lemma double_incr : forall n : nat, double (S n) = S (S (double n)).
+Proof.
+  intros n.
+  induction n as [|n IHn].
+  - reflexivity.
+  - {
+      rewrite -> IHn.
+      rewrite -> double_plus.
+      rewrite -> double_plus.
+      rewrite <- plus_n_Sm.
+      rewrite <- plus_n_Sm.
+      rewrite -> plus_Sn_m.
+      rewrite -> plus_Sn_m.
+      reflexivity.
+    }
+Qed.
+
+Definition double_bin (b:bin) : bin
+(* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+
+Example double_bin_zero : double_bin Z = Z.
+(* FILL IN HERE *) Admitted.
+
+Lemma double_incr_bin : forall b,
+    double_bin (incr b) = incr (incr (double_bin b)).
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+(* ************************************************************************** *)
+(* Exercise: 4 stars, advanced (bin_nat_bin) *)
+
+Fixpoint normalize (b:bin) : bin
+  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+
+Theorem bin_nat_bin : forall b, nat_to_bin (bin_to_nat b) = normalize b.
+Proof.
+  (* FILL IN HERE *) Admitted.
 
 (* ************************************************************************** *)
