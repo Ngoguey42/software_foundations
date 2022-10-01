a3c38ab766820b74d6ae876b19ddb44a37881867
Ngoguey42 <ngoguey@student.42.fr>
2022-10-01 16:36:46 +0200

    Finish MyInduction

--- lf/MyInduction.v
+++ lf/MyInduction.v
@@ -395,25 +395,265 @@ Proof.
     }
 Qed.
 
-Definition double_bin (b:bin) : bin
-(* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
-
-Example double_bin_zero : double_bin Z = Z.
-(* FILL IN HERE *) Admitted.
+(* Definition double_bin (b:bin) : bin := B0 b *)
+Definition double_bin (b:bin) : bin :=
+  match b with
+  | Z => Z
+  | _ => B0 b
+  end.
+Definition zero_bin : bin := Z.
+Definition one_bin : bin := B1 zero_bin.
+Definition two_bin : bin := B0 one_bin.
+Definition three_bin : bin := B1 one_bin.
+Definition four_bin : bin := B0 two_bin.
+Definition five_bin : bin := B1 two_bin.
+Definition six_bin : bin := B0 three_bin.
+Definition seven_bin : bin := B1 three_bin.
+Definition eight_bin : bin := B0 four_bin.
+Example test_bin_double0 : double_bin zero_bin = zero_bin. Proof. reflexivity. Qed.
+Example test_bin_double1 : double_bin one_bin = two_bin. Proof. reflexivity. Qed.
+Example test_bin_double2 : double_bin two_bin = four_bin. Proof. reflexivity. Qed.
+Example test_bin_double3 : double_bin three_bin = six_bin. Proof. reflexivity. Qed.
 
 Lemma double_incr_bin : forall b,
     double_bin (incr b) = incr (incr (double_bin b)).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros b.
+  induction b as [|b IHb|b IHb].
+  - reflexivity.
+  - {
+      simpl.
+      reflexivity.
+    }
+  - {
+      simpl.
+      reflexivity.
+    }
+Qed.
 
 (* ************************************************************************** *)
 (* Exercise: 4 stars, advanced (bin_nat_bin) *)
+Definition zero_nat : nat := O.
+Definition one_nat : nat := S zero_nat.
+Definition two_nat : nat := S one_nat.
+Definition three_nat : nat := S two_nat.
+Definition four_nat : nat := S three_nat.
+Definition five_nat : nat := S four_nat.
+Definition six_nat : nat := S five_nat.
+Definition seven_nat : nat := S six_nat.
 
-Fixpoint normalize (b:bin) : bin
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Definition zero_bin' : bin := B0 Z.
+Definition one_bin' : bin := B1 zero_bin'.
+Definition two_bin' : bin := B0 one_bin'.
+Definition three_bin' : bin := B1 one_bin'.
+Definition four_bin' : bin := B0 two_bin'.
+Definition five_bin' : bin := B1 two_bin'.
+Definition six_bin' : bin := B0 three_bin'.
+Definition seven_bin' : bin := B1 three_bin'.
+Definition eight_bin' : bin := B0 four_bin'.
+Module Again2.
+  Example test_0 : bin_to_nat zero_bin' = zero_nat. Proof. reflexivity. Qed.
+  Example test_1 : bin_to_nat one_bin' = one_nat. Proof. reflexivity. Qed.
+  Example test_2 : bin_to_nat two_bin' = two_nat. Proof. reflexivity. Qed.
+  Example test_3 : bin_to_nat three_bin' = three_nat. Proof. reflexivity. Qed.
+  Example test_4 : bin_to_nat four_bin' = four_nat. Proof. reflexivity. Qed.
+  Example test_5 : bin_to_nat five_bin' = five_nat. Proof. reflexivity. Qed.
+  Example test_6 : bin_to_nat six_bin' = six_nat. Proof. reflexivity. Qed.
+  Example test_7 : bin_to_nat seven_bin' = seven_nat. Proof. reflexivity. Qed.
+End Again2.
 
+Definition zero_bin'' : bin := B0 (B0 Z).
+Definition one_bin'' : bin := B1 zero_bin''.
+Definition two_bin'' : bin := B0 one_bin''.
+Definition three_bin'' : bin := B1 one_bin''.
+Definition four_bin'' : bin := B0 two_bin''.
+Definition five_bin'' : bin := B1 two_bin''.
+Definition six_bin'' : bin := B0 three_bin''.
+Definition seven_bin'' : bin := B1 three_bin''.
+Definition eight_bin'' : bin := B0 four_bin''.
+Module Again3.
+  Example test_0 : bin_to_nat zero_bin'' = zero_nat. Proof. reflexivity. Qed.
+  Example test_1 : bin_to_nat one_bin'' = one_nat. Proof. reflexivity. Qed.
+  Example test_2 : bin_to_nat two_bin'' = two_nat. Proof. reflexivity. Qed.
+  Example test_3 : bin_to_nat three_bin'' = three_nat. Proof. reflexivity. Qed.
+  Example test_4 : bin_to_nat four_bin'' = four_nat. Proof. reflexivity. Qed.
+  Example test_5 : bin_to_nat five_bin'' = five_nat. Proof. reflexivity. Qed.
+  Example test_6 : bin_to_nat six_bin'' = six_nat. Proof. reflexivity. Qed.
+  Example test_7 : bin_to_nat seven_bin'' = seven_nat. Proof. reflexivity. Qed.
+End Again3.
+
+Fixpoint insert_zeroes (b : bin) (zeroes : nat) : bin :=
+  match zeroes with
+  | O => b
+  | S zeroes => B0 (insert_zeroes b zeroes)
+  end.
+Fixpoint normalize' (b:bin) (zeroes:nat) : bin :=
+  match b with
+  | Z => Z
+  | B1 b => insert_zeroes (B1 (normalize' b 0)) zeroes
+  | B0 b => normalize' b (zeroes + 1)
+  end.
+Definition normalize (b:bin) : bin := normalize' b 0.
+Module Again4.
+  Example test_0 : normalize zero_bin' = zero_bin. Proof. reflexivity. Qed.
+  Example test_1 : normalize one_bin' = one_bin. Proof. reflexivity. Qed.
+  Example test_2 : normalize two_bin' = two_bin. Proof. reflexivity. Qed.
+  Example test_3 : normalize three_bin' = three_bin. Proof. reflexivity. Qed.
+  Example test_4 : normalize four_bin' = four_bin. Proof. reflexivity. Qed.
+  Example test_5 : normalize five_bin' = five_bin. Proof. reflexivity. Qed.
+  Example test_6 : normalize six_bin' = six_bin. Proof. reflexivity. Qed.
+  Example test_7 : normalize seven_bin' = seven_bin. Proof. reflexivity. Qed.
+End Again4.
+Module Again5.
+  Example test_0 : normalize zero_bin'' = zero_bin. Proof. reflexivity. Qed.
+  Example test_1 : normalize one_bin'' = one_bin. Proof. reflexivity. Qed.
+  Example test_2 : normalize two_bin'' = two_bin. Proof. reflexivity. Qed.
+  Example test_3 : normalize three_bin'' = three_bin. Proof. reflexivity. Qed.
+  Example test_4 : normalize four_bin'' = four_bin. Proof. reflexivity. Qed.
+  Example test_5 : normalize five_bin'' = five_bin. Proof. reflexivity. Qed.
+  Example test_6 : normalize six_bin'' = six_bin. Proof. reflexivity. Qed.
+  Example test_7 : normalize seven_bin'' = seven_bin. Proof. reflexivity. Qed.
+End Again5.
+
+(* Lemmas for B0 case of bin_nat_bin **************************************** *)
+Theorem b0_bin_to_nat : forall b, bin_to_nat (B0 b) = double (bin_to_nat b).
+Proof.
+  intros b.
+  induction b as [|b IHb|b IHb].
+  - reflexivity.
+  - {
+      simpl.
+      rewrite <- !plus_n_O.
+      rewrite -> double_plus.
+      reflexivity.
+    }
+  - {
+      simpl.
+      rewrite <- !plus_n_O.
+      rewrite -> double_plus.
+      reflexivity.
+    }
+Qed.
+Theorem double_nat_to_bin : forall n, nat_to_bin (double n) = double_bin (nat_to_bin n).
+Proof.
+  intros n.
+  induction n as [|n IHn].
+  - reflexivity.
+  - {
+      rewrite -> double_incr.
+      rewrite -> incr_and_nat_to_bin.
+      rewrite -> incr_and_nat_to_bin.
+      rewrite -> incr_and_nat_to_bin.
+      rewrite -> double_incr_bin.
+      rewrite -> IHn.
+      reflexivity.
+    }
+Qed.
+Theorem plus_one_in_insert_zeroes : forall b z, insert_zeroes b (z + 1) = B0 (insert_zeroes b z).
+Proof.
+  intros b z.
+  induction z as [|z' IHz'].
+  - simpl. reflexivity.
+  - simpl. rewrite <- IHz'. reflexivity.
+Qed.
+Theorem double_bin_is_plus1_in_normalize' :
+  forall b z, normalize' b (z + 1) = double_bin (normalize' b z).
+Proof.
+  intros b.
+  induction b as [|b' IHb'|b' IHb'].
+  - reflexivity.
+  - induction z as [|z' IHz'].
+    * simpl. rewrite <- IHb'. reflexivity.
+    * simpl. rewrite <- IHb'. reflexivity.
+  - induction z as [|z' IHz'].
+    * simpl. reflexivity.
+    * simpl. rewrite -> plus_one_in_insert_zeroes. reflexivity.
+Qed.
+
+(* Lemmas for B1 case of bin_nat_bin **************************************** *)
+Theorem b1_bin_to_nat : forall b, bin_to_nat (B1 b) = double (bin_to_nat b) + 1.
+Proof.
+  intros b.
+  induction b as [|b IHb|b IHb].
+  - reflexivity.
+  - {
+      simpl.
+      rewrite <- !plus_n_O.
+      rewrite -> double_plus.
+      reflexivity.
+    }
+  - {
+      simpl.
+      rewrite <- !plus_n_O.
+      rewrite -> double_plus.
+      reflexivity.
+    }
+Qed.
+Theorem b1_nat_to_bin : forall n, nat_to_bin (double n + 1) = B1 (nat_to_bin n).
+Proof.
+  intros n.
+  induction n as [|n IHn].
+  - reflexivity.
+  - {
+      rewrite -> double_incr.
+      simpl.
+      rewrite -> IHn.
+      simpl.
+      reflexivity.
+    }
+Qed.
+(* Theorem b1_is_double_plus_one : forall b, B1 b = incr (double_bin b). *)
+(* Proof. *)
+(*   intros b. *)
+(*   induction b as [|b' IHb'|b' IHb']. *)
+(*   - reflexivity. *)
+(*   - simpl. reflexivity. *)
+(*   - simpl. reflexivity. *)
+(* Qed. *)
+Theorem b1_around_normalize : forall b, B1 (normalize b) = normalize (B1 b).
+Proof.
+  intros b.
+  induction b as [|b' IHb'|b' IHb'].
+  - reflexivity.
+  - {
+      unfold normalize. (* alors la... *)
+      simpl.
+      reflexivity.
+    }
+  - {
+      unfold normalize. (* alors la... *)
+      simpl.
+      reflexivity.
+    }
+Qed.
+
+(* The GOAL ***************************************************************** *)
 Theorem bin_nat_bin : forall b, nat_to_bin (bin_to_nat b) = normalize b.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros b.
+  induction b as [|b IHb|b IHb].
+  - reflexivity.
+  - {
+      rewrite -> b0_bin_to_nat.
+      rewrite -> double_nat_to_bin.
+      rewrite -> IHb.
+
+      (* A partir de la c'est un peu de la merde, mais ca marche *)
+      unfold normalize.
+      simpl.
+      assert (H: 1 = 0 + 1). reflexivity.
+      rewrite -> H.
+      rewrite -> double_bin_is_plus1_in_normalize'.
+      reflexivity.
+    }
+  - {
+      rewrite -> b1_bin_to_nat.
+      rewrite -> b1_nat_to_bin.
+      rewrite -> IHb.
+
+      rewrite -> b1_around_normalize.
+      reflexivity.
+    }
+Qed.
 
 (* ************************************************************************** *)
